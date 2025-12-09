5910f545479a93e40f6ac65bd9b8955abb1750c5
Jon Koops <jonkoops@gmail.com>
2025-12-09 10:36:23 +0100

    Allow redirect URL to be different than app origin (#230)
    Closes #189
    
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- test/support/common.ts
+++ test/support/common.ts
@@ -2,6 +2,7 @@ export const APP_URL = new URL('http://localhost:3000')
 export const AUTH_SERVER_URL = new URL('http://localhost:8080')
 export const APP_URL_CROSS_ORIGIN = new URL('http://kjs-app.localhost:3000')
 export const AUTH_SERVER_URL_CROSS_ORIGIN = new URL('http://kjs-auth.localhost:8080')
+export const REDIRECT_SERVICE_URL = new URL('http://kjs-redirect.localhost:3000')
 export const ADMIN_USERNAME = 'admin'
 export const ADMIN_PASSWORD = 'admin'
 export const CLIENT_ID = 'keycloak-js-test-client'
