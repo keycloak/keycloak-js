5b59fd15ce3436095ab43ece1f550bb2cfeb7a74
Jon Koops <jonkoops@gmail.com>
2026-02-17 16:59:42 +0100

    Use host network for running tests on Podman
    Closes #268
    
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -7,7 +7,7 @@ const KEYCLOAK_VERSION = 'latest'
 export default defineConfig<TestOptions>({
   fullyParallel: true,
   webServer: [{
-    command: `podman run -p 8080:8080 -p 9000:9000 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin -e KC_HEALTH_ENABLED=true --pull=newer quay.io/keycloak/keycloak:${KEYCLOAK_VERSION} start-dev`,
+    command: `podman run --network=host -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin -e KC_HEALTH_ENABLED=true --pull=newer quay.io/keycloak/keycloak:${KEYCLOAK_VERSION} start-dev`,
     url: 'http://localhost:9000/health/live',
     stdout: 'pipe',
     reuseExistingServer: true,
